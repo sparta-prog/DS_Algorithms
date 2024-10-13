1e1caafcb882626cf6d66f3b38caa803af6fd087
sparta-prog <debarun285@gmail.com>
2024-10-13 10:08:27 +0530

    Create kthMissPositive.cpp

---
+++ BinarySearch/kthMissPositive.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        
+    }
+};
+
+int main() {
+    vector<int> data = {2,3,4,7,11};
+    int k = 5;
+    Solution obj;
+
+    cout<<obj.findKthPositive(data,k);
+}
